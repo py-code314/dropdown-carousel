0688aa45f75980d04c863458be0babecc239da94
srikk <122169588+py-code314@users.noreply.github.com>
2025-04-21 08:58:46 -0400

    style: Add styles to dropdown menu

--- src/scripts/dropdown-menu.js
+++ src/scripts/dropdown-menu.js
@@ -1,15 +1,15 @@
 // Import HTML elements
-const menuButton = document.querySelector('#menu-button')
-console.log(menuButton)
-const closeButton = document.querySelector('#close-button')
-const menu = document.querySelector('#menu')
+// const menuButton = document.querySelector('#menu-button')
+// // console.log(menuButton)
+// const closeButton = document.querySelector('#close-button')
+// const menu = document.querySelector('#menu')
 
-// Event listeners for menu buttons
-menuButton.addEventListener('click', () => {
-  console.log('menu btn clicked')
-  menu.classList.toggle('hidden')
-})
+// // Event listeners for menu buttons
+// menuButton.addEventListener('click', () => {
+//   console.log('menu btn clicked')
+//   menu.classList.toggle('hidden')
+// })
 
-closeButton.addEventListener('click', () => {
-  menu.classList.add('hidden')
-})
+// closeButton.addEventListener('click', () => {
+//   menu.classList.add('hidden')
+// })
