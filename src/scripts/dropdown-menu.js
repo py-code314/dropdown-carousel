9cbc8c1bc2f3e9a39e5de49fdeb6c598556eab5b
srikk <122169588+py-code314@users.noreply.github.com>
2025-04-21 09:11:48 -0400

    feat: Add JS code for dropdown

--- src/scripts/dropdown-menu.js
+++ src/scripts/dropdown-menu.js
@@ -1,14 +1,14 @@
 // Import HTML elements
-// const menuButton = document.querySelector('#menu-button')
-// // console.log(menuButton)
+const menuButton = document.querySelector('#menu-button')
+// console.log(menuButton)
 // const closeButton = document.querySelector('#close-button')
-// const menu = document.querySelector('#menu')
+const dropdown = document.querySelector('#dropdown')
 
-// // Event listeners for menu buttons
-// menuButton.addEventListener('click', () => {
-//   console.log('menu btn clicked')
-//   menu.classList.toggle('hidden')
-// })
+// Event listeners for menu buttons
+menuButton.addEventListener('click', () => {
+  console.log('menu btn clicked')
+  dropdown.classList.toggle('active')
+})
 
 // closeButton.addEventListener('click', () => {
 //   menu.classList.add('hidden')
