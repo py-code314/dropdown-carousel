488153bb5f3520d58e2f8b6366c0a2f43823e748
srikk <122169588+py-code314@users.noreply.github.com>
2025-04-22 15:36:45 -0400

    docs: Add comments

--- src/scripts/dropdown.js
+++ src/scripts/dropdown.js
@@ -4,17 +4,19 @@ const dropdownButtons = document.querySelectorAll('.button__dropdown')
 
 
 
-function openMenu() {
-  const currentDropdown = this.nextElementSibling
-  const isOpen = currentDropdown.classList.contains('open')
+/* Toggle the open state of the dropdown menu associated with the given button */
+function toggleDropdown() {
+  const dropdown = this.nextElementSibling;
+  const isDropdownOpen = dropdown.classList.contains('open');
 
-  dropdowns.forEach((dropdown) => dropdown.classList.remove('open'))
-  if (!isOpen) {
-    currentDropdown.classList.add('open')
+  dropdowns.forEach((dropdown) => dropdown.classList.remove('open'));
+  if (!isDropdownOpen) {
+    dropdown.classList.add('open');
   }
 }
 
+/* Event listener for button clicks */
 dropdownButtons.forEach((button) => {
-  button.addEventListener('click', openMenu)
+  button.addEventListener('click', toggleDropdown)
 })
 
